2c1627a173deaf406b911c908629f57e9b082b47
XudongSun <248381185@qq.com>
2018-04-14 16:59:56 +0800

    program report

--- ml2/code/oneVsRest.m
+++ ml2/code/oneVsRest.m
@@ -5,16 +5,20 @@ n = size(X, 2);
 
 
 all_theta = zeros(num_labels, n);
+all_theta2 = zeros(num_labels, n);
 
-
-%options = optimset('GradObj', 'on', 'MaxIter', 50, 'Display', 'off');
+options = optimset('GradObj', 'on', 'MaxIter', 50, 'Display', 'off');
 for c = 1:num_labels
     initial_theta = all_theta(c, :)';
+    initial_theta2 = all_theta2(c, :)';
     old_cost = costFunction(initial_theta, X, (y==c), lambda)
-    %[all_theta(c,:)] = fminunc (@(t)(costFunction(t, X, (y == c), lambda)), initial_theta, options);
+    [all_theta2(c,:)] = fminunc (@(t)(costFunction(t, X, (y == c), lambda)), initial_theta2, options);
     [all_theta(c,:)] = gradientDescent(X, (y==c), initial_theta, lambda,  alpha, num_iters);
     %all_theta(c,:)'
     new_cost = costFunction(all_theta(c, :)', X, (y==c), lambda)
+    new_cost2 = costFunction(all_theta2(c, :)', X, (y==c), lambda)
 end
 all_theta
+all_theta2
+all_theta - all_theta2
 end
