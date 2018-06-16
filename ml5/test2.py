646789758805e5f4820105ae6887813cc1ac11ab
XudongSun <248381185@qq.com>
2018-06-16 20:58:40 +0800

    ps5

---
+++ ml5/test2.py
@@ -0,0 +1,112 @@
+from sklearn import tree
+from sklearn import metrics
+from sklearn.model_selection import KFold
+import numpy as np
+
+class AdaBoost:
+
+    def __init__(self, n_estimators = 10):
+        self.h = list()
+        self.alpha = np.zeros(n_estimators)
+        self.n_estimators = n_estimators
+
+    def fit(self, XTrain, yTrain):
+        length = XTrain.shape[0]
+        D = np.ones(length) / length
+
+        for t in range(self.n_estimators):
+
+            clf = tree.DecisionTreeClassifier()
+            f = clf.fit(XTrain, yTrain, sample_weight=D)
+            pred = f.predict(XTrain)
+
+            error = np.mean(np.average(pred != yTrain, weights=D))
+
+            #print(error)
+
+            if error > 0.5:
+                break
+            elif error <= 0:
+                self.h.append(f)
+                self.alpha[t] = 1
+                break
+
+            self.h.append(f)
+
+            self.alpha[t] = 0.5 * np.log((1 - error) / error)
+            D = D*np.exp(-1*pred*yTrain*self.alpha[t])
+            D = D/np.sum(D)
+
+
+    def predict(self, XTest):
+        length = len(self.h)
+        pred = np.zeros((length, XTest.shape[0]))
+        result = np.zeros(XTest.shape[0])
+        prob = np.zeros(XTest.shape[0])
+
+        for t in range(0, length):
+            pred[t] = self.h[t].predict(XTest)*self.alpha[t]
+            prob += pred[t]
+
+
+
+        for i in range(XTest.shape[0]):
+            if prob[i] >= 0:
+                result[i] = 1
+            else:
+                result[i] = -1
+        return result, prob
+
+
+
+def loadData():
+    XTrain = np.genfromtxt("adult_dataset/adult_train_feature.txt")
+    XTest = np.genfromtxt("adult_dataset/adult_test_feature.txt")
+    yTrain = np.genfromtxt("adult_dataset/adult_train_label.txt")
+    yTest = np.genfromtxt("adult_dataset/adult_test_label.txt")
+    return XTrain, XTest, yTrain, yTest
+
+def crossValidation(XTrain, yTrain):
+    skf = KFold(n_splits=5)
+    maxT = 2
+    maxAuc = 0.0
+
+    for t in range(1, 51):
+        Auc = 0.0
+        for trIdx, cvIdx in skf.split(XTrain, yTrain):
+            # print(trIdx, cvIdx)
+            XTr, yTr = XTrain[trIdx], yTrain[trIdx]
+            XCV, yCV = XTrain[cvIdx], yTrain[cvIdx]
+            adaboost = AdaBoost(n_estimators = t)
+            adaboost.fit(XTr, yTr)
+            result, prob = adaboost.predict(XCV)
+            Auc += metrics.roc_auc_score(yCV, prob)
+        Auc = Auc / 5
+        if Auc > maxAuc:
+            maxT = t
+            maxAuc = Auc
+        print(t, Auc)
+
+    return maxT
+
+if __name__ == '__main__':
+
+    XTrain, XTest, yTrain, yTest = loadData()
+
+    yTrain = (yTrain - 0.5)*2
+    yTest = (yTest - 0.5)*2
+
+    T = crossValidation(XTrain, yTrain)
+    print(T)
+
+
+    adaboost = AdaBoost()
+    adaboost.fit(XTrain, yTrain)
+    yPred, yProb = adaboost.predict(XTest)
+
+
+    print("AdaBoost: ", metrics.roc_auc_score(yTest, yProb))
+
+
+
+
---
+++ ml5/test3.py
@@ -0,0 +1,96 @@
+from sklearn import tree
+from sklearn import metrics
+import numpy as np
+from random import randrange
+from sklearn.model_selection import KFold
+
+class RandomForest:
+
+    def __init__(self, n_estimators = 10):
+        self.h = list()
+        self.n_estimators = n_estimators
+
+    def sample(self, length):
+        index = np.zeros(length, dtype=np.int32)
+        for i in range(length):
+            r = randrange(0, length)
+            index[i] = r
+        return index
+
+    def fit(self, XTrain, yTrain):
+        for t in range(self.n_estimators):
+            clf = tree.ExtraTreeClassifier(max_features=4)
+            index = self.sample(XTrain.shape[0])
+            #print(index)
+            XSub = XTrain[index]
+            ySub = yTrain[index]
+
+            f = clf.fit(XSub, ySub)
+            self.h.append(f)
+
+    def predict(self, XTest):
+        length = XTest.shape[0]
+        result = np.zeros(length)
+        prob = np.zeros(length)
+        T = len(self.h)
+        for t in range(T):
+            pred = self.h[t].predict(XTest)
+            prob += pred
+
+        for i in range(length):
+            if prob[i] >= 0:
+                result[i] = 1
+            else:
+                result[i] = -1
+        return result, prob
+
+
+
+def loadData():
+    XTrain = np.genfromtxt("adult_dataset/adult_train_feature.txt")
+    XTest = np.genfromtxt("adult_dataset/adult_test_feature.txt")
+    yTrain = np.genfromtxt("adult_dataset/adult_train_label.txt")
+    yTest = np.genfromtxt("adult_dataset/adult_test_label.txt")
+    return XTrain, XTest, yTrain, yTest
+
+
+def crossValidation(XTrain, yTrain):
+    skf = KFold(n_splits=5)
+    maxT = 2
+    maxAuc = 0.0
+
+    for t in range(1, 51):
+        Auc = 0.0
+        for trIdx, cvIdx in skf.split(XTrain, yTrain):
+            # print(trIdx, cvIdx)
+            XTr, yTr = XTrain[trIdx], yTrain[trIdx]
+            XCV, yCV = XTrain[cvIdx], yTrain[cvIdx]
+            randomforest = RandomForest(n_estimators = t)
+            randomforest.fit(XTr, yTr)
+            result, yProb = randomforest.predict(XCV)
+            Auc += metrics.roc_auc_score(yCV, yProb)
+        Auc = Auc / 5
+        if Auc > maxAuc:
+            maxT = t
+            maxAuc = Auc
+        print(t, Auc)
+
+    return maxT
+
+
+if __name__ == '__main__':
+    XTrain, XTest, yTrain, yTest = loadData()
+    yTrain = (yTrain - 0.5)*2
+    yTest = (yTest - 0.5)*2
+
+    T = crossValidation(XTrain, yTrain)
+    print(T)
+
+    randomforest = RandomForest(n_estimators=T)
+    randomforest.fit(XTrain, yTrain)
+    yPred, yProb = randomforest.predict(XTest)
+
+    print("Random Forest: ", metrics.roc_auc_score(yTest, yProb))
+
+
+
